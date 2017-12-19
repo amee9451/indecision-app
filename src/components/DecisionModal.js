673ee546c083b2580096b134b6676b3719a4a915
Ben Thompson <git@iambenthompson.com>
2017-12-19 11:35:01 -0500

    Adds modal styling, with animation, hooking into React Modal package

--- src/components/DecisionModal.js
+++ src/components/DecisionModal.js
@@ -7,10 +7,12 @@ const DecisionModal = (props) => (
     <Modal
         isOpen={!!props.selectedOption}
         contentLabel="Selected Option"
+        closeTimeoutMS={200}
+        className="modal"
     >
-        <h3>Selected Option</h3>
-        {props.selectedOption && <p>{props.selectedOption}</p>}
-        <button onClick={props.clearDecision}>Okay</button>
+        <h3 className="modal__title">Selected Option</h3>
+        {props.selectedOption && <p className="modal__body">{props.selectedOption}</p>}
+        <button className="button" onClick={props.clearDecision}>Okay</button>
     </Modal>
 );
 
