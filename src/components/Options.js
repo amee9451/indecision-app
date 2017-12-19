1c6a6fe74d0e8e4564963ead746b3ed0451e2529
Ben Thompson <git@iambenthompson.com>
2017-12-19 08:53:02 -0500

    Converts the rest of the stateless components to implicit return syntax for brevity

--- src/components/Options.js
+++ src/components/Options.js
@@ -2,22 +2,20 @@ import React from 'react';
 
 import Option from './Option';
 
-const Options = (props) => {
-    return (
-        <div>
-            <button onClick={props.resetOptions}>Remove All</button>
-            {props.options.length === 0 && <p>Please add an option to get started!</p>}
-            <ol>
-                {props.options.map((option, index) => (
-                    <Option 
-                        key={index} 
-                        optionText={option} 
-                        deleteOption={props.deleteOption}
-                    />
-                ))}
-            </ol>
-        </div>
-    );
-};
+const Options = (props) => (
+    <div>
+        <button onClick={props.resetOptions}>Remove All</button>
+        {props.options.length === 0 && <p>Please add an option to get started!</p>}
+        <ol>
+            {props.options.map((option, index) => (
+                <Option 
+                    key={index} 
+                    optionText={option} 
+                    deleteOption={props.deleteOption}
+                />
+            ))}
+        </ol>
+    </div>
+);
 
 export default Options;
